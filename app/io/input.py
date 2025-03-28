354f2ab7fcd7f59830878776d2a3b89b6d6d5b4a
Oleksandra Malii <minelenova1@gmail.com>
2025-03-28 19:48:20 +0200

    Added input and output functions: console/file IO and pandas support

---
+++ app/io/input.py
@@ -0,0 +1,12 @@
+
+
+def user_input():
+    pass
+
+
+def file_input():
+    pass
+
+
+def pandas_input():
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,12 @@
+
+
+def user_output():
+    pass
+
+
+def file_output():
+    pass
+
+
+def pandas_output():
+    pass
